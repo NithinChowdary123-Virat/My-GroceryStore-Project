84be8e92d42628ff539fb4aabed658c60bf7a990
Nithin Chowdary <Nithin Yenikapati>
2026-07-20 15:49:45 +0530

    Update ALLOWED_HOSTS with live Render URL

--- backend/webapp/settings.py
+++ backend/webapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-weamia++z(su-_hb%z+ic
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['grocerystore-backend-clif.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
