79ee34ec1f93d64fb99db45fc718e04869b2bca9
Nithin Chowdary <Nithin Yenikapati>
2026-07-20 19:07:15 +0530

    Fix whitenoise static storage strict validation error

--- backend/webapp/settings.py
+++ backend/webapp/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [
 ]
 #Additional for deploying
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 MEDIA_URL = 'media/'
